63c30a3c1cd9c99ee359818d9059ecac357fd8b9
NicknEma <emanuele.rovini@gmail.com>
2025-05-31 11:59:02 +0200

    Fix compilation errors on Linux

--- fedit.c
+++ fedit.c
@@ -76,6 +76,7 @@
 # include <termios.h>
 # include <unistd.h>
 # include <sys/ioctl.h>
+# include <sys/mman.h>
 # include <sys/utsname.h>
 #else
 # error Platform not supported.
@@ -160,6 +161,7 @@ fsize(FILE *fp) {
 //- Integer/pointer/array/type manipulations
 
 #define array_count(a) (sizeof(a)/sizeof((a)[0]))
+#define array_count(a) (i64)(sizeof(a)/sizeof((a)[0]))
 
 #define bytes(n)     (   1ULL * n)
 #define kilobytes(n) (1024ULL * bytes(n))
@@ -332,8 +334,6 @@ mem_release(void *ptr, u64 size) {
 
 #elif OS_LINUX
 
-#include <sys/mman.h>
-
 static void *
 mem_reserve(u64 size) {
 	void *result = mmap(0, size, 0, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
@@ -344,21 +344,26 @@ mem_reserve(u64 size) {
 
 static void *
 mem_commit(void *ptr, u64 size) {
-	int r = mprotect(data, size, PROT_READ|PROT_WRITE);
+	int r = mprotect(ptr, size, PROT_READ|PROT_WRITE);
 	assert(r != -1);
 	
 	return ptr;
 }
 
-static bool
-mem_decommit(void *data, u64 size) {
-	(void)mprotect(data, size, 0);
-	(void)madvise(data, size, MADV_FREE);
+static void *
+mem_reserve_and_commit(u64 size) {
+	return mem_commit(mem_reserve(size), size);
 }
 
 static bool
-mem_release(data: rawptr, size: uint) {
-	(void)munmap(data, size);
+mem_decommit(void *ptr, u64 size) {
+	return (mprotect(ptr, size, 0) != -1 &&
+			madvise(ptr, size, MADV_FREE) != -1);
+}
+
+static bool
+mem_release(void *ptr, u64 size) {
+	return munmap(ptr, size) != -1;
 }
 
 #endif
